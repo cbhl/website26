423c3f6006f5700206a951f3f8a44371843b1def
Michael Chang <thenewme91@gmail.com>
2026-02-01 05:34:26 -0500

    Add reboot endpoint and deploy trigger
    Expose /rebootz to exit with code 42 and call it after deployment to restart the service.

--- main.go
+++ main.go
@@ -9,9 +9,16 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello, world!"))
 }
 
+func rebootz(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("Rebooting..."))
+    os.Exit(42)
+}
+
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", helloWorld)
+    mux.HandleFunc("/rebootz", rebootz)
 
     port := os.Getenv("PORT")
     if port == "" {
